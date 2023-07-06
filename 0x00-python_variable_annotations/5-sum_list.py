0cc3176ccc691dc3f9f876c23c95d01ea333f76a
bentalla108 <tallambaye108@gmail.com>
2023-07-06 22:58:38 +0000

    Ok for commited

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Function: sum_list()
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Function: sum_list()
+
+    Description:
+    Calculates the sum of all the elements in the input list.
+
+    Parameters:
+    - input_list (list[float]): A list of float numbers.
+
+    Return Type:
+    float: The sum of all the elements in the input list.
+    """
+
+    return sum(input_list)
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Function: sum_list()
+"""
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Function: sum_list()
+
+    Description:
+    Calculates the sum of all the elements in the input list.
+
+    Parameters:
+    - input_list (list[float]): A list of float numbers.
+
+    Return Type:
+    float: The sum of all the elements in the input list.
+    """
+
+    return sum(mxd_lst)
+    

39dc29d36aedbcd8a254bc0b60f663834027d5df
bentalla108 <tallambaye108@gmail.com>
2023-07-06 23:16:22 +0000

    Turple tortue

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+    Function: to_kv()
+
+"""
+
+
+def to_kv(k: str, v: int | float) -> tuple:
+    """
+    Function: to_kv()
+
+    Description:
+    Converts a key-value pair into a tuple.
+
+    Parameters:
+    - k (str): The key.
+    - v (int or float): The value, which can be either an integer or a float.
+
+    Return Type:
+    tuple: A tuple containing the key-value pair.
+    """
+
+    return (k, v)

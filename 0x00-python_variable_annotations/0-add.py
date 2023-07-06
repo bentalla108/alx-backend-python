2643daf4561ab86bf10dd28c6bc527daf921b1c1
bentalla108 <tallambaye108@gmail.com>
2023-07-06 08:50:35 +0000

    Python anotation another kind of codind'

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+def add(a: float, b: float) -> float:
+    """
+    Function: add()
+
+    Description:
+    The add() function is a Python function that takes two integer arguments a and b and returns their sum as a floating-point number.
+
+    Parameters:
+    - a (int): The first integer argument to be added.
+    - b (int): The second integer argument to be added.
+
+    Return Type:
+    float: The sum of a and b as a floating-point number.
+
+    The add() function is specifically designed to work with integer arguments and returns the sum as a float to accommodate cases where the sum may result in a non-integer value.
+    """
+    return a + b

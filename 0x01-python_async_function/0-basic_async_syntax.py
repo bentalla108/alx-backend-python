b99c2bcd306d1e7af3f58457a0ed57f027ca9c70
bentalla108 <tallambaye108@gmail.com>
2023-07-10 21:13:47 +0000

    Is a promess .?

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+Basic async syntax module
+"""
+import asyncio
+import random
+
+async def wait_random(max_delay=10):
+    """
+    Asynchronous function that generates a random delay and waits for that duration.
+
+    Args:
+        max_delay (int): The maximum delay value. Defaults to 10.
+
+    Returns:
+        int: The random delay value.
+
+    """
+    rng = random.randrange(max_delay)
+    await asyncio.sleep(rng)
+    return rng
